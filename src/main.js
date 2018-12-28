a110a1b6f8191dad449ab7133dc0727a532c23e6
Matheus Meira <meira.matheus.rob@gmail.com>
2018-12-28 11:32:17 -0200

    Add husky

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const a = 3;
+const b = 4
+const c = 'asdaksjdbasbdiasdiuaiudoajsoidaonsdoansodnaisudnoasndoasndoansdjnajsdnaojsndoansdoansdouansouodnaosndasd';
+function main() {
+  return a + b + c;
+}
+main();
